0fea55ce650d564750df60649bb4024ca0d730c3
bbha99 <branha99@gmail.com>
2023-02-13 10:18:09 -0800

    add food item queries

--- db/queries/users.js
+++ db/queries/users.js
@@ -7,4 +7,19 @@ const getUsers = () => {
     });
 };
 
-module.exports = { getUsers };
+const checkUserRole = (id) => {
+  console.log("id", id)
+  return db.query('SELECT * FROM users WHERE id = $1;', [id])
+  .then(data => {
+    return data.rows[0];
+  });
+}
+
+const getFoodItems = () => {
+  return db.query('SELECT * FROM foods;')
+    .then(data => {
+      return data.rows;
+    });
+};
+
+module.exports = { getUsers, checkUserRole, getFoodItems };
