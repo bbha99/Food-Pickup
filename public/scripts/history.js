194502828bab04a71d11dafaa1df7365ee2def9c
bbha99 <branha99@gmail.com>
2023-02-13 23:00:07 -0800

    display order history

---
+++ public/scripts/history.js
@@ -0,0 +1,43 @@
+// Client facing scripts here
+$(() => {
+  $.ajax({
+    method: 'GET',
+    url: '/api/users/order'
+  })
+  .then((response) => {
+    console.log("response", response.orders)
+    console.log("response2", response.orderIds[2])
+    const orderIds = response.orderIds;
+    const orderData = response.orders;
+
+    $orders = $(".orders");
+    $orders.empty();
+
+    // Display the orders to the admin
+    for (let i = orderIds.length - 1; i >= 0; i--) {
+      const currentId = orderIds[i].order_id;
+      let $currentOrderDetails =  ``;
+      $orders.append($($currentOrderDetails));
+      let totalCost = 0;
+      let orderStatus = "";
+      for (const data of orderData) {
+        if (currentId === data.order_id) {
+          totalCost+= data.price / 100 * data.quantity;
+          $currentOrderDetails += `
+            <p>Order Id: ${data.order_id}</p>
+            <p>Name: ${data.name}</p>
+            <p>Price: $${data.price / 100 * data.quantity}</p>
+            <p>Quantity: ${data.quantity}</p>
+            <p>user_id: ${data.user_id}</p>
+            `
+          orderStatus = data.order_status;
+        }
+      }
+      $currentOrder =`<div class="order-item">` + $currentOrderDetails + `<p>Order Status: ${orderStatus}</p> <p>Total Cost: $${totalCost}</p>` + `</div>`;
+      $orders.append($currentOrder);
+    }
+  });
+
+
+
+});
