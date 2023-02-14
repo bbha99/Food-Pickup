08ab758ae5725e722be7d13c71209d4f96678635
bbha99 <branha99@gmail.com>
2023-02-13 17:55:33 -0800

    create order routes

---
+++ routes/order.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router  = express.Router();
+
+// twilio configurations
+const twilio = require('twilio');
+
+const accountSid = `${process.env.TWILIO_ACCOUNTSID}`; // Your Account SID from www.twilio.com/console
+const authToken = `${process.env.TWILIO_AUTHTOKEN}` // Your Auth Token from www.twilio.com/console
+
+const client = require('twilio')(accountSid, authToken);
+
+const orderQueries = require('../db/queries/orders');
+
+// Notify user by SMS order is pending
+function pendingTwilio() {
+  client.messages
+  .create({
+    body: 'Order is processing. We will give you an estimate of the time remaining soon!',
+    to: process.env.PHONE, // Text this number
+    from: `${process.env.TWILIO_PHONE}`, // From a valid Twilio number
+  })
+  .then((message) => console.log(message.sid));
+}
+
+router.get('/', (req, res) => {
+  const userId = req.session.user_id;
+  const userRole = req.session.user_role;
+
+  if (userRole === 'customer') {
+    const orderId = req.query.orderId;
+    console.log("this is orderid:", orderId)
+    res.render("order_confirmed", {orderId});
+  } else {
+    res.redirect('../');
+  }
+
+
+
+});
+
+router.post('/', (req, res) => {
+  const userId = req.session.user_id;
+  const userRole = req.session.user_role;
+  const cartData = req.body.cartItems;
+
+  if (userRole === 'customer') {
+
+    // Create a new order
+    orderQueries.addOrder(userId)
+    .then(orderData => {
+      const orderId = orderData.id;
+
+      // pendingTwilio();
+
+      // Add items and their quantities to order cart
+      for (const cartItem in cartData) {
+        if (cartData[cartItem].quantity > 0) {
+          orderQueries.addItemToOrder(cartData[cartItem], orderId)
+          .then(res => {
+            console.log("added successfully: ", res);
+          });
+        }
+      }
+      res.json({ orderId });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({result: "error"});
+    });
+  }
+});
+
+module.exports = router;
