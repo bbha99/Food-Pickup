c7fa9dc2af201f0d5518455fe2da47d5b8e385d2
bbha99 <branha99@gmail.com>
2023-02-16 15:35:39 -0800

    remove comments and fix spacing

--- db/queries/foods.js
+++ db/queries/foods.js
@@ -2,71 +2,47 @@ const db = require('../connection');
 
 const getFoodItems = () => {
   return db
-  .query(`
-  SELECT *
-  FROM foods
-  ORDER BY id DESC;
-  `)
-  .then(result => {
-  return result.rows;
-  })
-  .catch(err => console.error('query error', err.stack));
-  };
-
-const getFoodByOwnerId = (id) => {
-  return db
-  .query(`
-  SELECT *
-  FROM foods
-  WHERE owner_id = $1;
-  `, [id])
-  .then(result => {
-  console.log(result.rows);
-  return result.rows[0];
-  })
-  .catch((err) => {
-    return null;
-  });
+    .query(`
+    SELECT *
+    FROM foods
+    ORDER BY id DESC;
+    `)
+    .then(result => {
+      return result.rows;
+    })
+    .catch(err => console.error('query error', err.stack));
 };
 
 const addFoodItem = (food) => {
-  console.log("food", food);
-  console.log("food.price", food.price)
   let values = [food.name, food.description, food.imageUrl, Math.round(food.price * 100), food.userId];
 
   return db
-  .query(`
+    .query(`
   INSERT INTO foods (name, description, image_url, price, owner_id)
   VALUES ($1, $2, $3, $4, $5)
   RETURNING *;
   `, values)
-  .then((result) => {
-    console.log("result.rows", result.rows);
-    return result.rows[0];
-  })
-  .catch((err) => {
-    console.log("error", err);
-    return null;
-  });
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log("error", err);
+      return null;
+    });
 };
 
 const getToggleValue = (id) => {
-  console.log("id", id)
   return db.query(`SELECT * FROM foods WHERE id = $1`, [id])
-  .then(toggleValue => {
-    console.log("toggleValue", toggleValue.rows[0]);
-    return (toggleValue.rows[0]);
-  });
+    .then(toggleValue => {
+      return (toggleValue.rows[0]);
+    });
 };
 
 const toggleItem = (id, status) => {
-  console.log("in db query ",id)
-  console.log("statusstatus", status);
   return db.query(`UPDATE foods SET toggle = $1 WHERE id = $2 RETURNING *;`, [status, id])
-  .then(updatedData => {
-    // console.log("we are in the toggled Query then block updatedData", updatedData);
-    return (updatedData.rows[0]);
-  });
+    .then(updatedData => {
+      return (updatedData.rows[0]);
+    });
 };
 
-module.exports = { getFoodItems, getFoodByOwnerId, addFoodItem, toggleItem, getToggleValue };
+module.exports = { getFoodItems, addFoodItem, toggleItem, getToggleValue };
--- db/queries/users.js
+++ db/queries/users.js
@@ -2,74 +2,75 @@ const db = require('../connection');
 
 const getUsers = () => {
   return db
-  .query(`
+    .query(`
   SELECT *
   FROM users;
   `)
-  .then(result => {
-  console.log(result.rows);
-  return result.rows;
-  })
-  .catch(err => console.error('query error', err.stack));
-  };
+    .then(result => {
+      return result.rows;
+    })
+    .catch(err => console.error('query error', err.stack));
+};
 
 const getUserWithId = (id) => {
   return db
-  .query(`
+    .query(`
   SELECT *
   FROM users
   WHERE id = $1;
   `, [id])
-  .then(result => {
-  console.log(result.rows);
-  return result.rows[0];
-  })
-  .catch((err) => {
-    return null;
-  });
+    .then(result => {
+      console.log(result.rows);
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.error('query error', err);
+      return null;
+    });
 };
 
 const getUsersByRole = (role) => {
   return db
-  .query(`
+    .query(`
   SELECT *
   FROM users
   WHERE role = $1;
   `, [role])
-  .then(result => {
-  console.log(result.rows);
-  return result.rows;
-  })
-  .catch((err) => {
-    return null;
-  });
+    .then(result => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.error('query error', err);
+      return null;
+    });
 };
 
 const addUser =  function(user) {
   let values = [user.name, user.role];
 
   return db
-  .query(`
+    .query(`
   INSERT INTO users (name, role)
   VALUES ($1, $2)
   RETURNING *;
   `, values)
-  .then((result) => {
-    console.log(result.rows);
-    return result.rows[0];
-  })
-  .catch((err) => {
-    console.error(null);
-    return null;
-  });
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.error('query error', err);
+      return null;
+    });
 };
 
 const checkUserRole = (id) => {
-  console.log("id", id)
+  console.log("id", id);
   return db.query('SELECT * FROM users WHERE id = $1;', [id])
-  .then(data => {
-    return data.rows[0];
-  });
-}
+    .then(data => {
+      return data.rows[0];
+    });
+};
 
 module.exports = { getUsers, getUserWithId, getUsersByRole, addUser, checkUserRole };
