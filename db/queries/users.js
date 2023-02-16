733e7f2111a97b5ffbac01f7f9f5f56446ecd22d
bbha99 <branha99@gmail.com>
2023-02-16 15:39:03 -0800

    remove unused queries

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,5 +1,6 @@
 const db = require('../connection');
 
+// Returns all user data
 const getUsers = () => {
   return db
     .query(`
@@ -12,65 +13,12 @@ const getUsers = () => {
     .catch(err => console.error('query error', err.stack));
 };
 
-const getUserWithId = (id) => {
-  return db
-    .query(`
-  SELECT *
-  FROM users
-  WHERE id = $1;
-  `, [id])
-    .then(result => {
-      console.log(result.rows);
-      return result.rows[0];
-    })
-    .catch((err) => {
-      console.error('query error', err);
-      return null;
-    });
-};
-
-const getUsersByRole = (role) => {
-  return db
-    .query(`
-  SELECT *
-  FROM users
-  WHERE role = $1;
-  `, [role])
-    .then(result => {
-      console.log(result.rows);
-      return result.rows;
-    })
-    .catch((err) => {
-      console.error('query error', err);
-      return null;
-    });
-};
-
-const addUser =  function(user) {
-  let values = [user.name, user.role];
-
-  return db
-    .query(`
-  INSERT INTO users (name, role)
-  VALUES ($1, $2)
-  RETURNING *;
-  `, values)
-    .then((result) => {
-      console.log(result.rows);
-      return result.rows[0];
-    })
-    .catch((err) => {
-      console.error('query error', err);
-      return null;
-    });
-};
-
+// Returns the user data associated with the id
 const checkUserRole = (id) => {
-  console.log("id", id);
   return db.query('SELECT * FROM users WHERE id = $1;', [id])
     .then(data => {
       return data.rows[0];
     });
 };
 
-module.exports = { getUsers, getUserWithId, getUsersByRole, addUser, checkUserRole };
+module.exports = { getUsers, checkUserRole };
